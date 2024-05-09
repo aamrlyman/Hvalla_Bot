1ffa215806ad79b5d7a09618567f3f9da6146cdd
aamrlyman <aamrlyman@gmail.com>
2024-05-08 22:36:26 -0700

    add tests and logtic for parsing user input

--- tests/parse_user_input.test.ts
+++ tests/parse_user_input.test.ts
@@ -1,70 +1,66 @@
 import {
   getActivityFromInput,
   activities,
+  getPropertyFromInput,
+  getBonusFromInput,
 } from "../src/logic/parse_user_input";
 
 import { Character, Bonus } from "../src/Data/character_info";
 import { Activity } from "../src/Data/character_info";
+import { userInputs, userInputPropertyTests } from "./user_input_testcases";
 
-export const userInputs = [
-  `EXPLORING
-
-Zone: Thuelheim Mountains
-Important Area: Hallen Stone
-Character ID and Name: W28 Sigelblyse
-Activity-specific Bonuses: 
-- Forn Gevir
-- Raven `,
-  `HUNTING
-
-Zone: Forest of Lime
-Important Area: Eastern Ley-Well
-Prey: Clipper Ant
-Character ID and Name: W8 Morioch
-Activity-specific Bonuses: 
-- Forn Gevir
-- Screech Owl`,
-
-  `SCAVENGING
-
-Zone: Utgard
-Important Area: Ravenstone Village
-Character ID and Name: W69 Fellheim
-Activity-specific Bonuses: 
-- Forn Gevir`,
-
-  `SCAVENGING 
-
+const inputNoActivity = `scaveging 
+  
 Zone: Utgard 
 Important Area: Ravenstone Village 
 Character ID and Name: W69 Fellheim 
-Activity-specific Bonuses: 
-- Forn Gevir`,
-  `
-SCAVENGING 
-
-Zone: Utgard 
-Important Area: Ravenstone Village 
-Character ID and Name: W69 Fellheim 
-Activity-specific Bonuses: 
-- Forn Gevir`,
-  `
-SCAVEGING 
-
-Zone: Utgard 
-Important Area: Ravenstone Village 
-Character ID and Name: W69 Fellheim 
-Activity-specific Bonuses: 
-- Forn Gevir`,
-];
+Activity-specific Bonuses:`;
 
 describe("test getActivityFromInput", () => {
-  test("Make sure function returns enum type", () => {
-    for (const input of userInputs) {
-      const activity = getActivityFromInput(input);
-      console.log(activity);
-      expect(activities).toContain(activity);
-    }
+  userInputs.forEach((testCase) => {
+    test(testCase.name, () => {
+      const activity = getActivityFromInput(testCase.input) as Activity;
+      expect(activities.includes(activity)).toBe(true);
+    });
+  });
+  test("No Activity", () => {
+    const activity = getActivityFromInput(inputNoActivity);
+    expect(activity).toBeUndefined();
+  });
+});
+
+const inputNoBonuses = `scaveging 
+  
+Zone: Utgard 
+Important Area: Ravenstone Village 
+Character ID and Name: W69 Fellheim 
+Activity-specific Bonuses:`;
+
+describe("test getActivityFromInput", () => {
+  userInputs.forEach((testCase) => {
+    test(testCase.name, () => {
+      const bonusList = getBonusFromInput(testCase.input);
+      expect(
+        bonusList.every((item) => Object.values(Bonus).includes(item))
+      ).toBe(true);
+    });
+    test("Test no bonuses", () => {
+      const bonusList = getBonusFromInput(inputNoBonuses);
+      expect(bonusList).toEqual([]);
+    });
+  });
+});
+
+const propertyInputs: string[] = ["zone", "area", "prey"];
+
+describe("test getActivityFromInput", () => {
+  userInputPropertyTests.forEach((testCase) => {
+    test(testCase.name, () => {
+      propertyInputs.forEach((property) => {
+        const propertyValue = getPropertyFromInput(testCase.input, property);
+        expect(propertyValue).toEqual(testCase.expected[property]);
+      });
+    });
   });
 });
 
---
+++ tests/user_input_testcases.ts
@@ -0,0 +1,137 @@
+export interface userInputsTestCases {
+  input: string;
+  name: string;
+}
+
+export const userInputs: userInputsTestCases[] = [
+  {
+    input: `EXPLORING
+  
+  Zone: Thuelheim Mountains
+  Important Area: Hallen Stone
+  Character ID and Name: W28 Sigelblyse
+  Activity-specific Bonuses: 
+  - Forn Gevir
+  - Raven `,
+    name: "Test 1: Exploring Standard input",
+  },
+  {
+    input: `HUNTING
+  
+  Zone: Forest of Lime
+  Important Area: Eastern Ley-Well
+  Prey: Clipper Ant
+  Character ID and Name: W8 Morioch
+  Activity-specific Bonuses: 
+  - Forn Gevir
+  - Screech Owl`,
+    name: "Test 2: Hunting standard input",
+  },
+  {
+    input: `SCAVENGING
+    
+    Zone: Utgard
+    Important Area: Ravenstone Village
+    Character ID and Name: W69 Fellheim
+    Activity-specific Bonuses: 
+    - Forn Gevir`,
+    name: "Test 3: Scavenging input",
+  },
+  {
+    input: `
+    SCAVENGING 
+  
+  Zone: Utgard 
+  Important Area: Ravenstone Village 
+  Character ID and Name: W69 Fellheim 
+  Activity-specific Bonuses: 
+  - Forn Gevir`,
+    name: "Test 4: Scavenging input with extra spaces",
+  },
+  {
+    input: `scaveging 
+  
+  Zone: Utgard 
+  Important Area: Ravenstone Village 
+  Character ID and Name: W69 Fellheim 
+  Activity-specific Bonuses: 
+  - Forn Gevir`,
+    name: "Test 5: Scavenging input mispelled and lower case",
+  },
+  {
+    input: `
+  ScAVaNGING 
+  
+  Zone: Utgard 
+  Important Area: Ravenstone Village 
+  Character ID and Name: W69 Fellheim 
+  Activity-specific Bonuses: 
+  - Forn Gevir`,
+    name: "Test 6: Scavenging input extra spaces upper and lower case",
+  },
+  {
+    input: `
+      huntings            
+  
+  Zone: Utgard 
+  Important Area: Ravenstone Village 
+  Character ID and Name: W69 Fellheim 
+  Activity-specific Bonuses: 
+  - Forn Gevir`,
+    name: "Test 7: hunting input extra spaces and mispelled",
+  },
+];
+
+export interface UserInputPropertyTestCases {
+  name: string;
+  input: string;
+  expected: {
+    zone: string;
+    area: string;
+    id: string;
+    name: string;
+    prey: string | null;
+  };
+}
+
+export const userInputPropertyTests: UserInputPropertyTestCases[] = [
+  {
+    name: "Test 1: Hunting Standard input",
+    input: `
+    huntings            
+
+Zone: Utgard 
+Important Area: Ravenstone Village 
+Character ID and Name: W69 Fellheim
+prey: Goat
+Activity-specific Bonuses: 
+- Forn Gevir`,
+
+    expected: {
+      zone: "Utgard",
+      area: "Ravenstone Village",
+      id: "W69",
+      name: "Fellheim",
+      prey: "Goat",
+    },
+  },
+  {
+    name: "Test 2: Hunting different Order",
+    input: `
+    Exploration            
+
+    Character ID and Name: SGG123 Bob 
+    Important Area: Coalminster 
+    Zone: Thuelheim Mountains 
+    Activity-specific Bonuses: 
+    - Forn Gevir`,
+
+    expected: {
+      zone: "Thuelheim Mountains",
+      area: "Coalminster",
+      id: "SGG123",
+      name: "Bob",
+      prey: null,
+    },
+  },
+];
