87c8d8b5ef5d288b4b9bcad052374403fa343c04
rickReachReporting <rick@reachreporting.com>
2024-05-10 20:53:42 -0700

    formatting changes

--- src/Logic/injury_outcome.ts
+++ src/Logic/injury_outcome.ts
@@ -1,23 +1,28 @@
 import { generateRandNum, valueRangeMapper } from "./shared_functions";
 import { injuriesInfo, InjuriesInfo } from "../Data/injuries_data";
 
-
-export function injuryOutcome(rolls: number[], injuriesInfo: InjuriesInfo): string {
-    if (!isInjured(rolls[0], injuriesInfo.injuredThreshold)) {
-        return "No Injury";
-    }
-    if (isMinorInjury(rolls[1], injuriesInfo.minorInjuryThreshold)) {
-        return `Minor | -${generateRandNum(20)} HP`;
-    } else {
-        const majorInjury = valueRangeMapper(rolls[2], injuriesInfo.majorInjuries);
-        return `Major - ${majorInjury}`;
-    }
+export function injuryOutcome(
+  rolls: number[],
+  injuriesInfo: InjuriesInfo
+): string {
+  if (!isInjured(rolls[0], injuriesInfo.injuredThreshold)) {
+    return "No Injury";
+  }
+  if (isMinorInjury(rolls[1], injuriesInfo.minorInjuryThreshold)) {
+    return `Minor | -${generateRandNum(20)} HP`;
+  } else {
+    const majorInjury = valueRangeMapper(rolls[2], injuriesInfo.majorInjuries);
+    return `Major - ${majorInjury}`;
+  }
 }
 
-function isMinorInjury(diceRoll: number, minorInjuryThreshold: number): boolean {
-    return diceRoll < minorInjuryThreshold;
+function isMinorInjury(
+  diceRoll: number,
+  minorInjuryThreshold: number
+): boolean {
+  return diceRoll < minorInjuryThreshold;
 }
 
 function isInjured(diceRoll: number, injuredThreshold: number): boolean {
-    return diceRoll > injuredThreshold;
+  return diceRoll > injuredThreshold;
 }
