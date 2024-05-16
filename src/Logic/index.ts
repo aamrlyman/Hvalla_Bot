c0f0f8f887a3d411ad485723ec5e800b7d1b9c34
rickReachReporting <rick@reachreporting.com>
2024-05-15 21:00:10 -0700

    update runActivity() to take in a string

--- src/Logic/index.ts
+++ src/Logic/index.ts
@@ -2,8 +2,7 @@ import { main } from "./main";
 import { Character } from "../Data/character_info";
 import { createCharacterFromInput } from "./parse_user_input";
 
-export function runActivity(): string {
-  const input = document.getElementById("activityInput").value;
+export function runActivity(input: string): string {
   const character: Character | string = createCharacterFromInput(input);
   if (typeof character === "string") {
     return character;
