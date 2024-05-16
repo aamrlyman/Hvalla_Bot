b2a585eabbb755500a0e04fda744197c754fab39
rickReachReporting <rick@reachreporting.com>
2024-05-15 19:52:41 -0700

    move test cases to data folder

---
+++ src/Data/user_input_testcases.ts
@@ -0,0 +1,200 @@
+export interface userInputsTestCases {
+  input: string;
+  name: string;
+}
+
+export const userInputs: userInputsTestCases[] = [
+  {
+    name: "Test 1: Exploring Standard input",
+    input: `EXPLORING
+  
+  Zone: Thuelheim Mountains
+  Important Area: Hallen Stone
+  Character ID and Name: W28 Sigelblyse
+  Activity-specific Bonuses: 
+  - Forn Gavir
+  - Raven `,
+  },
+  {
+    name: "Test 2: Hunting standard input",
+    input: `HUNTING
+  
+  Zone: Forest of Lime
+  Important Area: Eastern Ley-Well
+  Prey: Clipper Ant
+  Character ID and Name: W8 Morioch
+  Activity-specific Bonuses: 
+  - Forn Gavir
+  - Screech Owl`,
+  },
+  {
+    name: "Test 3: Scavenging input",
+    input: `SCAVENGING
+    
+    Zone: Utgard
+    Important Area: Ravenstone Village
+    Character ID and Name: W69 Fellheim
+    Activity-specific Bonuses: 
+    - Forn Gavir`,
+  },
+  {
+    name: "Test 4: Scavenging input with extra spaces",
+    input: `
+    SCAVENGING 
+  
+  Zone: Utgard 
+  Important Area: Ravenstone Village 
+  Character ID and Name: W69 Fellheim 
+  Activity-specific Bonuses: 
+  - Forn Gavir`,
+  },
+  {
+    name: "Test 5: Scavenging input mispelled and lower case",
+    input: `scaveging 
+  
+  Zone: Utgard 
+  Important Area: Ravenstone Village 
+  Character ID and Name: W69 Fellheim 
+  Activity-specific Bonuses: 
+  - Forn Gavir`,
+  },
+  {
+    name: "Test 6: Scavenging input extra spaces upper and lower case",
+    input: `
+  ScAVaNGING 
+  
+  Zone: Utgard 
+  Important Area: Ravenstone Village 
+  Character ID and Name: W69 Fellheim 
+  Activity-specific Bonuses: 
+  - Forn Gavir`,
+  },
+  {
+    name: "Test 7: hunting input extra spaces and mispelled",
+    input: `
+      huntings            
+  
+  Zone: Utgard 
+  Important Area: Ravenstone Village 
+  Character ID and Name: W69 Fellheim 
+  Activity-specific Bonuses: 
+  - Forn Gavir
+  - Grey Owl`,
+  },
+];
+
+export interface UserInputPropertyTestCases {
+  name: string;
+  input: string;
+  expected: {
+    zone: string;
+    area: string;
+    id: string;
+    name: string;
+    prey: string | null | undefined;
+  };
+}
+
+export const userInputPropertyTests: UserInputPropertyTestCases[] = [
+  {
+    name: "Test 1: Hunting Standard input",
+    input: `
+    huntings            
+
+Zone: Utgard 
+Important Area: Ravenstone Village 
+Character ID and Name: W69 Fellheim
+prey: Goat
+Activity-specific Bonuses: 
+- Forn Gavir`,
+
+    expected: {
+      zone: "Utgard",
+      area: "Ravenstone Village",
+      id: "W69",
+      name: "Fellheim",
+      prey: "Goat",
+    },
+  },
+  {
+    name: "Test 2: Hunting different Order",
+    input: `
+    exploration            
+
+    character id and name: SGG123 Bob 
+    important Area: Coalminster 
+    zone: Thuelheim Mountains 
+    Activity-specific Bonuses: 
+    - Forn Gavir`,
+
+    expected: {
+      zone: "Thuelheim Mountains",
+      area: "Coalminster",
+      id: "SGG123",
+      name: "Bob",
+      prey: undefined,
+    },
+  },
+];
+
+interface BadUserInput {
+  name: string;
+  input: string;
+  expected: string;
+}
+export const badUserInputs: BadUserInput[] = [
+  {
+    name: "Test 1: hunting input extra spaces and mispelled",
+    input: `
+      huuntings            
+  
+  Zone: Utgard 
+  Important Area: Ravenstone Village 
+  Character ID and Name: W69 Fellheim 
+  Activity-specific Bonuses: 
+  - Forn Gavir
+  - Grey Owl`,
+    expected: "Error: activity not found. Check spelling and formatting.",
+  },
+  {
+    name: "Test 2: Colon missing in zone",
+    input: `
+      huntings            
+  
+  zone Utgard 
+  Important Area: Ravenstone Village 
+  Character ID and Name: W69 Fellheim 
+  Activity-specific Bonuses: 
+  - Forn Gavir
+  - Grey Owl`,
+    expected:
+      'Error: zone not found. zone name and value must be separated by a colon ":"',
+  },
+  {
+    name: "Test 3: Colon missing in area",
+    input: `
+      huntings            
+  
+  zone: Utgard 
+  Important Area Ravenstone Village 
+  Character ID and Name: W69 Fellheim 
+  Activity-specific Bonuses: 
+  - Forn Gavir
+  - Grey Owl`,
+    expected:
+      'Error: area not found. area name and value must be separated by a colon ":"',
+  },
+  {
+    name: "Test 4: Areaa mispelled",
+    input: `
+      huntings            
+  
+  zone: Utgard 
+  Important Axreaa: Ravenstone Village 
+  Character ID and Name: W69 Fellheim 
+  Activity-specific Bonuses: 
+  - Forn Gavir
+  - Grey Owl`,
+    expected: "Error: area not found. Check spelling and formatting.",
+  },
+];
